a4def84688f6fb61fa7318e98b237150ff0e2b20
M.Spears <ms@ms.com>
2022-02-27 01:00:24 -0500

    updates

--- updated_recipe_search/search.js
+++ updated_recipe_search/search.js
@@ -1,9 +1,13 @@
 
+
 function searchRecipes(searchTerm, recipesArray) {
-  let data = {};
-  data.results = recipesArray.filter( r =>
-                       r.recipename.toLowerCase().includes(searchTerm) );
-  data.count = data.results.length;
+  let data = { count: 0 };
+  // perform only if searchTerm is truthy, rotect against an empty searchTerm
+  if (searchTerm) {
+    searchTerm = searchTerm.toLowerCase();
+    data.results = recipesArray.filter( r => r.recipename.toLowerCase().includes(searchTerm) );
+    data.count = data.results.length;
+  }
   return data;
 }
 
